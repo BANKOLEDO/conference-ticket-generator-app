fa26b0748a3448d9336c9ded1fb1cdafab9a7802
Bankole David <bankydavid12@gmail.com>
2025-07-10 21:17:16 +0100

    fixed slow image

--- src/App.jsx
+++ src/App.jsx
@@ -16,34 +16,31 @@ bg-neutral-900 md:bg-[url('/assets/images/background-desktop.png')]">
       {/* FORM OR TICKET */}
       {!ticketData ? (
         <div id="form-container" className="w-full relative z-20 px-4">
-          <FormSection
-  onGenerate={(data) => {
-    setTicketData(data);
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }}
-/>
+          <FormSection onGenerate={(data) => {
+            setTicketData(data);
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }}
+          />
         </div>
       ) : (
         <div id="ticket-container" className="w-full max-w-lg px-4">
-          <TicketSection
-  data={ticketData}
-  onReset={() => {
-    setTicketData(null);
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }}
-/>
+          <TicketSection data={ticketData}
+            onReset={() => {
+            setTicketData(null);
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }}
+          />
         </div>
       )}
 
       {/* Squiggly bottom pattern */}
-     <div className=" absolute z-1 bottom-0 left-0 max-w-full h-auto pointer-events-none">
-       <img
-  id="pattern-s-bottom"
-  src="/assets/images/pattern-squiggly-line-bottom-mobile-tablet.svg"
-  alt="pattern-s-bottom"
-/>
-     </div>
-
+      <div className=" absolute z-1 bottom-0 left-0 max-w-full h-auto pointer-events-none">
+        <img
+          id="pattern-s-bottom"
+          src="/assets/images/pattern-squiggly-line-bottom-mobile-tablet.svg"
+          alt="pattern-s-bottom"
+        />
+      </div>
 
       <Attribution />
     </main>
